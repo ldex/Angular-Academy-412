df0b6d23b8949d3fd664ada5d4b092775a741edb
ldex <laurent.duveau+git@gmail.com>
2024-10-18 10:23:28 -0400

    http + pagination

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../../models/product.interface';
 import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../../services/product.service';
+import { Observable } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-list',
@@ -16,9 +18,50 @@ export class ProductListComponent {
 
   title: string = 'Products'
 
+  // Pagination
+  pageSize = 5
+  start = 0
+  end = this.pageSize
+  pageNumber = 1
+
+  previousPage() {
+    this.start -= this.pageSize
+    this.end -= this.pageSize
+    this.pageNumber--
+    this.selectedProduct = null
+  }
+
+  nextPage() {
+    this.start += this.pageSize
+    this.end += this.pageSize
+    this.pageNumber++
+    this.selectedProduct = null
+  }
+
   private productService = inject(ProductService)
 
-  products: Product[] = this.productService.getProducts()
+  products: Product[];
+
+  constructor() {
+    this
+    .productService
+    .products$
+    .pipe(
+      takeUntilDestroyed()
+    )
+    .subscribe(
+      {
+        next: data => this.products = data,
+        error: err => console.error(err.message)
+      }
+     )
+  }
+
+
+  products$: Observable<Product[]> = this.productService.products$
+
+
+
 
   onSelect(product: Product) {
     this.selectedProduct = product
